e5ea6b099a1c02b21d1d0a763d13cc95b4579d1c
Mindy Kim <mkwashere913@gmail.com>
2021-07-26 22:18:03 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+PROJECT_NAME = "instrument_detection"  # change this!
+
+setuptools.setup(
+    name=PROJECT_NAME,
+    version="1.0",
+    packages=setuptools.find_packages(),
+)
